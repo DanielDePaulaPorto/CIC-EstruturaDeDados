49921fa3a8c3d93e01742109be13eb86df289571
Daniel de Paula Porto <danielpporto@gmail.com>
2024-10-15 11:56:28 -0300

    FEAT: Adicionados exercícios de revisão C

--- Exemplos/Recursao/ConversorBase.py
+++
@@ -1,9 +0,0 @@
-def toStr(n, base):
-    convertString = "0123456789ABCDEF"
-    if n < base:
-        return convertString[n]
-    else:
-        return toStr(n // base, base) + convertString[n % base]
-
-print(toStr(45,16))
-
